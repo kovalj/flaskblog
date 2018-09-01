b489fd84d15451df8642eebadd9cb74868b2425d
Yurii Koval <13747753+kovalj@users.noreply.github.com>
2018-09-01 14:32:01 +0300

    implemented login and registration forms, added templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'b7c803efad40e8b6cbdcecdc4b42eb5a'
 
 posts = [
     {
@@ -28,6 +31,27 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for { form.username.data }!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Log In', form=form)
 
 
 if __name__ == '__main__':
