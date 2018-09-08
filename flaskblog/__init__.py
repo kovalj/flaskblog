3c2dfe8c1752d5975fb6b76ec95ed2cde7f2d6fc
kovalj <koval.ur@gmail.com>
2018-09-09 02:37:11 +0300

    implemented password hashing using flask-bcrypt extension

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'b7c803efad40e8b6cbdcecdc4b42eb5a'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 
 from flaskblog import routes
