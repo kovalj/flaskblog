bfc108f96169968c5cd0f79b44f7ae7600b2d320
Yurii Koval <13747753+kovalj@users.noreply.github.com>
2018-09-09 18:30:56 +0300

    implemented login/logout functionality and a session management using flask-login extension

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -8,6 +9,7 @@ app.config['SECRET_KEY'] = 'b7c803efad40e8b6cbdcecdc4b42eb5a'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 
 from flaskblog import routes
